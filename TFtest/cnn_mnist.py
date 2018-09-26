39b7f42e82c175f6a62ece953d1ef2d8d24a6861
vonru <enjay0714@gmail.com>
2018-09-26 16:31:39 -0400

    started CNN tutorial

---
+++ TFtest/cnn_mnist.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf 
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+
+if __name__ == "main":
+    tf.app.run()
+
